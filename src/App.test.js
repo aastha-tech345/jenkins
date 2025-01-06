b17b732aefcd0b72f0abde330b4422c1b97f5e15
dluser5 <aasthamodanwal.dl@gmail.com>
2025-01-06 11:28:24 +0530

    save

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react123/i);
+  const linkElement = screen.getByText(/React/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("here test content", () => {
-  render(<App />);
-  const text = screen.getByText(/aastha213/i);
-  expect(text).toBeInTheDocument();
 });
